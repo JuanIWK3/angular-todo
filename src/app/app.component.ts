82082f973f9136fd21f5a7979f8f70b128a6fac1
Juan Israel dos Anjos <juaniwk3@gmail.com>
2022-02-16 02:02:52 -0300

    new task, localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  tasks: Array<{ name: string }> = JSON.parse(
+    localStorage.getItem('tasks') || '[]'
+  );
+
+  addTaskForm = this.formBuilder.group({
+    name: '',
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    if (
+      this.addTaskForm.value.name !== '' &&
+      this.addTaskForm.value.name !== null
+    ) {
+      console.log(this.addTaskForm.value);
+      this.tasks.push(this.addTaskForm.value);
+      this.addTaskForm.reset();
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
 }
