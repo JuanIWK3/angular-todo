2899f8032559248d387123cf4889b4c52a84701f
Juan Israel dos Anjos <juaniwk3@gmail.com>
2022-02-17 14:14:47 -0300

    check only one task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,23 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  tasks: Array<{ name: string }> = JSON.parse(
+  tasks: Array<{ name: string; checked: boolean }> = JSON.parse(
     localStorage.getItem('tasks') || '[]'
   );
 
-  checkbox: boolean = false;
-
-  check(event: Event) {
-    let input = event.target as HTMLInputElement;
-
-    if (input.checked) {
-      this.checkbox = true; // disable patch
-    } else {
-      this.checkbox = false;
-    }
-  }
-
   addTaskForm = this.formBuilder.group({
     name: '',
+    checked: false,
   });
 
   constructor(private formBuilder: FormBuilder) {}
+
+  check(event: Event, i: number) {
+    let checked = (event.target as HTMLInputElement).checked;
+
+    this.tasks[i].checked = checked;
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 
   deleteTask(event: number): void {
     this.tasks.splice(event, 1);
@@ -39,7 +35,6 @@ export class AppComponent {
       this.addTaskForm.value.name !== '' &&
       this.addTaskForm.value.name !== null
     ) {
-      console.log(this.addTaskForm.value);
       this.tasks.push(this.addTaskForm.value);
       this.addTaskForm.reset();
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
